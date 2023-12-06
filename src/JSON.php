e415698a0dbcebcc081673917fbce4a3bc2dbbe4
Dmitry <40324321+inilim@users.noreply.github.com>
2023-12-06 22:31:44 +0300

    Update JSON.php

--- src/JSON.php
+++ src/JSON.php
@@ -6,15 +6,68 @@ class JSON
 {
    public function isJSON(string $value): bool
    {
-      json_decode($value);
+      $this->decode($value);
       return !$this->hasError();
    }
 
    public function isJSONAsArray(string $value): bool
    {
-      $decode = json_decode($value, true);
+      $value = $this->decode($value);
       if ($this->hasError()) return false;
-      return is_array($decode);
+      return is_array($value);
+   }
+
+   public function isJSONAsInteger(string $value): bool
+   {
+      $value = $this->decode($value);
+      if ($this->hasError()) return false;
+      return is_int($value);
+   }
+
+   public function isJSONAsString(string $value): bool
+   {
+      $value = $this->decode($value);
+      if ($this->hasError()) return false;
+      return is_string($value);
+   }
+
+   public function isJSONAsFloat(string $value): bool
+   {
+      $value = $this->decode($value);
+      if ($this->hasError()) return false;
+      return is_float($value);
+   }
+
+   /**
+    * @param mixed[]|array{} $default
+    * @return mixed[]|array{}
+    */
+   public function tryDecodeAsArray(string $value, array $default = []): array
+   {
+      $value = $this->decode($value);
+      if (is_array($value)) return $value;
+      return $default;
+   }
+
+   public function tryDecodeAsString(string $value, string $default = ''): string
+   {
+      $value = $this->decode($value);
+      if (is_string($value)) return $value;
+      return $default;
+   }
+
+   public function tryDecodeAsInteger(string $value, int $default = 0): int
+   {
+      $value = $this->decode($value);
+      if (is_int($value)) return $value;
+      return $default;
+   }
+
+   public function tryDecodeAsFloat(string $value, float $default = 0.0): float
+   {
+      $value = $this->decode($value);
+      if (is_float($value)) return $value;
+      return $default;
    }
 
    // ------------------------------------------------------------------
@@ -24,5 +77,13 @@ class JSON
    protected function hasError(): bool
    {
       return json_last_error() !== JSON_ERROR_NONE;
+   }
+
+   /**
+    * @return mixed
+    */
+   protected function decode(string $value)
+   {
+      return json_decode($value, true);
    }
 }
