5d418eae321e28a4184d8de4505ce1d745393b03
Dmitry <40324321+inilim@users.noreply.github.com>
2023-12-13 00:28:41 +0300

    Update JSON.php

--- src/JSON.php
+++ src/JSON.php
@@ -2,37 +2,55 @@
 
 namespace Inilim\JSON;
 
+use stdClass;
+
 class JSON
 {
-   public function isJSON(string $value): bool
+   public function isJSON(?string $value): bool
    {
+      if (is_null($value)) return false;
       $this->decode($value);
       return !$this->hasError();
    }
 
-   public function isJSONAsArray(string $value): bool
+   /**
+    * @param boolean $strict true если вы ожидаете именно массив "[...]" | false если вы ожидаете массив или обьект как массив "[...]"|"{...}"
+    */
+   public function isJSONAsArray(?string $value, bool $strict = false): bool
    {
-      $value = $this->decode($value);
+      if (is_null($value)) return false;
+      $value = $this->decode($value, (!$strict ? true : false));
       if ($this->hasError()) return false;
       return is_array($value);
    }
 
-   public function isJSONAsInteger(string $value): bool
+   public function isJSONAsObject(?string $value): bool
    {
+      if (is_null($value)) return false;
+      $value = $this->decode($value, false);
+      if ($this->hasError()) return false;
+      return is_object($value);
+   }
+
+   public function isJSONAsInteger(?string $value): bool
+   {
+      if (is_null($value)) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
       return is_int($value);
    }
 
-   public function isJSONAsString(string $value): bool
+   public function isJSONAsString(?string $value): bool
    {
+      if (is_null($value)) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
       return is_string($value);
    }
 
-   public function isJSONAsFloat(string $value): bool
+   public function isJSONAsFloat(?string $value): bool
    {
+      if (is_null($value)) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
       return is_float($value);
@@ -42,29 +60,41 @@ class JSON
     * @param mixed[]|array{} $default
     * @return mixed[]|array{}
     */
-   public function tryDecodeAsArray(string $value, array $default = []): array
+   public function tryDecodeAsArray(?string $value, array $default = []): array
    {
+      if (is_null($value)) return $default;
       $value = $this->decode($value);
       if (is_array($value)) return $value;
       return $default;
    }
 
-   public function tryDecodeAsString(string $value, string $default = ''): string
+   public function tryDecodeAsObject(?string $value, object $default = new stdClass): object
    {
+      if (is_null($value)) return $default;
+      $value = $this->decode($value, false);
+      if (is_object($value)) return $value;
+      return $default;
+   }
+
+   public function tryDecodeAsString(?string $value, string $default = ''): string
+   {
+      if (is_null($value)) return $default;
       $value = $this->decode($value);
       if (is_string($value)) return $value;
       return $default;
    }
 
-   public function tryDecodeAsInteger(string $value, int $default = 0): int
+   public function tryDecodeAsInteger(?string $value, int $default = 0): int
    {
+      if (is_null($value)) return $default;
       $value = $this->decode($value);
       if (is_int($value)) return $value;
       return $default;
    }
 
-   public function tryDecodeAsFloat(string $value, float $default = 0.0): float
+   public function tryDecodeAsFloat(?string $value, float $default = 0.0): float
    {
+      if (is_null($value)) return $default;
       $value = $this->decode($value);
       if (is_float($value)) return $value;
       return $default;
@@ -82,8 +112,12 @@ class JSON
    /**
     * @return mixed
     */
-   protected function decode(string $value)
-   {
-      return json_decode($value, true);
+   protected function decode(
+      string $value,
+      bool $associative = true,
+      int $depth = 512,
+      int $flags = 0
+   ) {
+      return json_decode($value, $associative, $depth, $flags);
    }
 }
