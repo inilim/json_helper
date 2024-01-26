2be3a00295b2a100f3e66f1db98a1d256ea922f3
Dmitry <40324321+inilim@users.noreply.github.com>
2024-01-27 00:40:24 +0300

    ref

--- src/JSON.php
+++ src/JSON.php
@@ -8,7 +8,7 @@ class JSON
 {
    public function isJSON(?string $value): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $this->decode($value);
       return !$this->hasError();
    }
@@ -18,42 +18,42 @@ class JSON
     */
    public function isJSONAsArray(?string $value, bool $strict = false): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $value = $this->decode($value, (!$strict ? true : false));
       if ($this->hasError()) return false;
-      return is_array($value);
+      return \is_array($value);
    }
 
    public function isJSONAsObject(?string $value): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $value = $this->decode($value, false);
       if ($this->hasError()) return false;
-      return is_object($value);
+      return \is_object($value);
    }
 
    public function isJSONAsInteger(?string $value): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
-      return is_int($value);
+      return \is_int($value);
    }
 
    public function isJSONAsString(?string $value): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
-      return is_string($value);
+      return \is_string($value);
    }
 
    public function isJSONAsFloat(?string $value): bool
    {
-      if (is_null($value)) return false;
+      if ($value === null) return false;
       $value = $this->decode($value);
       if ($this->hasError()) return false;
-      return is_float($value);
+      return \is_float($value);
    }
 
    /**
@@ -62,41 +62,41 @@ class JSON
     */
    public function tryDecodeAsArray(?string $value, array $default = []): array
    {
-      if (is_null($value)) return $default;
+      if ($value === null) return $default;
       $value = $this->decode($value);
-      if (is_array($value)) return $value;
+      if (\is_array($value)) return $value;
       return $default;
    }
 
    public function tryDecodeAsObject(?string $value, object $default = new stdClass): object
    {
-      if (is_null($value)) return $default;
+      if ($value === null) return $default;
       $value = $this->decode($value, false);
-      if (is_object($value)) return $value;
+      if (\is_object($value)) return $value;
       return $default;
    }
 
    public function tryDecodeAsString(?string $value, string $default = ''): string
    {
-      if (is_null($value)) return $default;
+      if ($value === null) return $default;
       $value = $this->decode($value);
-      if (is_string($value)) return $value;
+      if (\is_string($value)) return $value;
       return $default;
    }
 
    public function tryDecodeAsInteger(?string $value, int $default = 0): int
    {
-      if (is_null($value)) return $default;
+      if ($value === null) return $default;
       $value = $this->decode($value);
-      if (is_int($value)) return $value;
+      if (\is_int($value)) return $value;
       return $default;
    }
 
    public function tryDecodeAsFloat(?string $value, float $default = 0.0): float
    {
-      if (is_null($value)) return $default;
+      if ($value === null) return $default;
       $value = $this->decode($value);
-      if (is_float($value)) return $value;
+      if (\is_float($value)) return $value;
       return $default;
    }
 
@@ -105,10 +105,10 @@ class JSON
     */
    public function getTypeFromJSON(?string $value): ?string
    {
-      if (is_null($value)) return null;
+      if ($value === null) return null;
       $value = $this->decode($value, false);
       if ($this->hasError()) return null;
-      return gettype($value);
+      return \gettype($value);
    }
 
    // ------------------------------------------------------------------
@@ -117,7 +117,7 @@ class JSON
 
    protected function hasError(): bool
    {
-      return json_last_error() !== JSON_ERROR_NONE;
+      return \json_last_error() !== JSON_ERROR_NONE;
    }
 
    /**
@@ -129,6 +129,6 @@ class JSON
       int $depth = 512,
       int $flags = 0
    ) {
-      return json_decode($value, $associative, $depth, $flags);
+      return \json_decode($value, $associative, $depth, $flags);
    }
 }
